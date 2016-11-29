a7df80affeb226d593449643f729e0456867ec48
Muthu Selvan <muthuselvan@Muthus-MacBook-Pro.local>
2016-11-28 21:41:24 -0800

    Second commit

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,12 @@
 /*
-Authod : muthu.selvan
+Author : muthu.selvan
+
 */
 public class GitDemo
 {
     public static void main(String args[]) {
         System.out.println("Initial commit ");
+		System.out.println("Second commit ");
     }
 
 }
