323b955177336c07b762caf03ac9abd523c3b4b1
Muthu Selvan <muthuselvan@Muthus-MacBook-Pro.local>
2016-11-28 21:48:03 -0800

    Third

--- GitDemo.java
+++ GitDemo.java
@@ -5,8 +5,9 @@ Author : muthu.selvan
 public class GitDemo
 {
     public static void main(String args[]) {
-        System.out.println("Initial commit ");
-		System.out.println("Second commit ");
+        System.out.println("Initial commit");
+		System.out.println("Second commit");
+		System.out.println("Third commit");
     }
 
 }
